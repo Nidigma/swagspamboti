60c253a91f913ecb4e01d6d11b0d58a53ebaa59e
Nidigma <57100020+Nidigma@users.noreply.github.com>
2020-02-16 19:27:03 +0700

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+const VK_KEY = "4ced93968ae02d5ca4a449d5c2e054bc85f7844a1f7daf43ca1c5e86866de9226b1eb68cdca6b4988ed85";
+const ACCESS_KEY = "d509129b";
+const VERSION = "5.89";
+const BTN_1 = [["command" => 'btn_1'], "Свег есть?", "green"];
+const BTN_2 = [["command" => 'btn_2'], "Спам!", "red"];
+const BTN_3 = [["command" => 'btn_3'], "Бот есть?", "white"];
