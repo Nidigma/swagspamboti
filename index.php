d67e51c8c4d8da0a983eeb1f62d60c2216b1bbe8
Nidigma <57100020+Nidigma@users.noreply.github.com>
2020-02-17 20:21:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,48 @@ $vk->sendOK();
 $peer_id = $data->object->peer_id; 
 $id = $data->object->from_id; 
 $message = $data->object->text; 
-if ($data->type == 'message_new') { 
+if ($data->type == 'message_new')
+	
+$vk = new vk_api(VK_KEY, VERSION); // создание экземпляра класса работы с api, принимает ключ и версию api
+$data = json_decode(file_get_contents('php://input')); //Получает и декодирует JSON пришедший из ВК
+//print_r($data);
+if ($data->type == 'confirmation') { //Если vk запрашивает ключ
+    exit(ACCESS_KEY); //Завершаем скрипт отправкой ключа
+}
+$vk->sendOK(); //Говорим vk, что мы приняли callback
+// Создаем необходимые переменные
+$peer_id = $data->object->peer_id; // Узнаем ИД беседы 2000000.....
+$id = $data->object->from_id; // Узнаем ид пользователя который отправляет команду
+$message = $data->object->text; // Текст самого сообщения
+$is_admin = [87444494, 183657]; // создаем массив с ID's наших будущих админов через запятую
+$chat_id = $peer_id - 2000000000;
+
+if ($data->type == 'message_new') { // Если это новое сообщение то выполняем код указанный в условии
+
+
+    if (mb_substr($message,0,5) == '/kick'){ // Обрезаем сообщение и сравниваем что получилось
+
+            if (in_array($id, $is_admin)) { // С помощью in_array проверяем схожесть переменной $id с массивом с ID's
+
+        $kick_id = mb_substr($message ,6); // еще раз обрезаем и получаем все что написано после /kick_
+        $kick_id = explode("|", mb_substr($kick_id, 3))[0];
+
+        if($kick_id == ""){
+            $vk->sendMessage($peer_id, "Вы забыли указать аргумент");
+
+        } else {
+
+        $vk->request('messages.removeChatUser', ['chat_id' => $chat_id, 'member_id' => $kick_id]);
+        $vk->sendMessage($peer_id, "id - {$kick_id} был исключен :-)");
+
+    }
+    } else {
+            $vk->sendMessage($peer_id, "У Вас нет доступа к этой команде!");
+
+        }
+    }
+}
+	{ 
 	switch ($message) {
         default:
             $vk->sendButton($peer_id, "Я готов к работе",[[BTN_1, BTN_2, BTN_3, BTN_4]]);
