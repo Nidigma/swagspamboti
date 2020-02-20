d1614d515346827a46fb69e26116c96260658a13
Nidigma <57100020+Nidigma@users.noreply.github.com>
2020-02-20 19:37:56 +0700

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
-const VK_KEY = "4ced93968ae02d5ca4a449d5c2e054bc85f7844a1f7daf43ca1c5e86866de9226b1eb68cdca6b4988ed85";
-const ACCESS_KEY = "d509129b";
+const VK_KEY = "92a47b67ffaef2f259efd942adbde02a9a60c797a5c3271d30aa184272b0a18b8fe57d8c68963778d7b08";
+const ACCESS_KEY = "716a4144";
 const VERSION = "5.89";
 const BTN_1 = [["command" => 'btn_1'], "Свег есть?", "green"];
 const BTN_2 = [["command" => 'btn_2'], "Спам!", "red"];
