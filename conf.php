c83070addf0053f0737caaeda6da932ed749ebb5
Nidigma <57100020+Nidigma@users.noreply.github.com>
2020-06-08 20:10:24 +0700

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
 const VK_KEY = "92a47b67ffaef2f259efd942adbde02a9a60c797a5c3271d30aa184272b0a18b8fe57d8c68963778d7b08";
-const ACCESS_KEY = "716a4144";
+const ACCESS_KEY = "98dbe1ce";
 const VERSION = "5.89";
 const BTN_1 = [["command" => 'btn_1'], "Свег есть?", "green"];
 const BTN_2 = [["command" => 'btn_2'], "Спам!", "red"];
